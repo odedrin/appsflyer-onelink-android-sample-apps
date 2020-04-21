84a297e19e9463e91e2509c55d9cc9a4ca7a5370
liaz.kamper@appsflyer.com <liaz.kamper>
2020-04-21 23:42:58 +0300

    Java Simple app: add dynamic dispatch

--- java/simple_deep_link_pass_data/app/src/main/java/com/appsflyer/onelink/appsflyeronelinkbasicapp/MainActivity.java
+++ java/simple_deep_link_pass_data/app/src/main/java/com/appsflyer/onelink/appsflyeronelinkbasicapp/MainActivity.java
@@ -37,9 +37,9 @@ public class MainActivity extends AppCompatActivity {
                 if(status.equals("Non-organic")){
                     if( Objects.requireNonNull(conversionData.get("is_first_launch")).toString().equals("true")){
                         Log.d(LOG_TAG,"Conversion: First Launch");
-                        if (conversionData.containsKey("item_id")){
+                        if (conversionData.containsKey("fruit_name")){
                             Log.d(LOG_TAG,"Conversion: This is deferred deep linking.");
-                            // TODO - match the input types
+                            //  TODO SDK - match the input types
                             Map<String,String> newMap = new HashMap<>();
                             for (Map.Entry<String, Object> entry : conversionData.entrySet()) {
                                 if(entry.getValue() instanceof String){
@@ -65,11 +65,23 @@ public class MainActivity extends AppCompatActivity {
             public void onAppOpenAttribution(Map<String, String> conversionData) {
                 for (String attrName : conversionData.keySet())
                     Log.d(LOG_TAG, "Deeplink attribute: " + attrName + " = " + conversionData.get(attrName));
-                Log.d(LOG_TAG, "Deep linking into " + conversionData.get("section_id"));
-                String targetSectionTag = conversionData.get("section_id") + "_section_fragment";
-                int targetId = getResources().getIdentifier(targetSectionTag, "id", getPackageName());
+                Log.d(LOG_TAG, "Deep linking into " + conversionData.get("fruit_name"));
                 Bundle dp_args = new Bundle();
-                dp_args.putString("item_id", conversionData.get("item_id"));
+                dp_args.putString("fruit_amount", conversionData.get("fruit_amount"));
+                String fruitName = conversionData.get("fruit_name");
+                switch (fruitName) {
+                    case "apple":
+                        goToApples(findViewById(R.id.apples_button));
+                        break;
+                    case "banana":
+                        goToBananas(findViewById(R.id.bananas_button));
+                        break;
+                    case "peach":
+                        goToPeaches(findViewById(R.id.peaches_button));
+                        break;
+                    default:
+                        Log.d(LOG_TAG, "Could not deep link into " + conversionData.get("target_fruit"));
+                }
             }
 
             @Override
@@ -80,17 +92,17 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void goToApples(View view) {
-        Intent intent = new Intent(this, applesActivity.class);
+        Intent intent = new Intent(getApplicationContext(), applesActivity.class);
         startActivity(intent);
     }
 
     public void goToBananas(View view) {
-        Intent intent = new Intent(this, bananasActivity.class);
+        Intent intent = new Intent(getApplicationContext(), bananasActivity.class);
         startActivity(intent);
     }
 
     public void goToPeaches(View view) {
-        Intent intent = new Intent(this, peachesActivity.class);
+        Intent intent = new Intent(getApplicationContext(), peachesActivity.class);
         startActivity(intent);
     }
 }
