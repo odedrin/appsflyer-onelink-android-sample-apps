2166d0f80caf9debbddfe77ff6ee665acfa6f597
liaz.kamper@appsflyer.com <liaz.kamper>
2020-05-01 18:44:13 +0300

    Java Simple app: Remove scrollable option and Copyable

--- java/simple_deep_link_pass_data/app/src/main/java/com/appsflyer/onelink/appsflyeronelinkbasicapp/applesActivity.java
+++ java/simple_deep_link_pass_data/app/src/main/java/com/appsflyer/onelink/appsflyeronelinkbasicapp/applesActivity.java
@@ -4,8 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.method.ScrollingMovementMethod;
-import android.util.Log;
 import android.widget.TextView;
 
 import java.util.HashMap;
@@ -23,14 +21,12 @@ public class applesActivity extends AppCompatActivity {
         HashMap<String, String> dlAttrMap = (HashMap<String, String>) intent.getSerializableExtra(MainActivity.DL_ATTRS);
         if (dlAttrMap != null) {
             TextView dlAttrsText = findViewById(R.id.apples_deeplinkparams);
-            dlAttrsText.setMovementMethod(new ScrollingMovementMethod());
             String dlAttrStrings = "";
             for (String attrName : dlAttrMap.keySet()) {
                 String deepLinkAttrStr = attrName + " = " + dlAttrMap.get(attrName) + "\n";
                 dlAttrStrings += deepLinkAttrStr;
             }
             dlAttrsText.setText(dlAttrStrings);
-            dlAttrsText.setMovementMethod(new ScrollingMovementMethod());
         } else {
             TextView dlTitleText = findViewById(R.id.apples_deeplinktitle);
             dlTitleText.setText("No Deep Linking Happened");
